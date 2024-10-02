b6baec6c91f116348d4131aa44779201338d8524
gipsoong <ryangipsoong@gmail.com>
2024-10-02 22:59:48 +0800

    Format and remove unnecessary code. Add more words to the print string for further clarity.

--- main.py
+++ main.py
@@ -10,13 +10,10 @@ c = 100
 print(a * b / c)
 end = timer()
 
-# print(end-start)
-total = (timedelta(seconds=end - start)).total_seconds()
-print(f'#1. {total}' + ' second(s)')
 
 def getDuration():
     duration = (timedelta(seconds=end - start)).total_seconds()
-    print(f'#2. {duration}' + ' second(s)')
+    print(f'Executed in: {duration}' + ' second(s)')
 
 
 getDuration()
