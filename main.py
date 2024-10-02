e8b581c98b0f496077a85b37d236f772fe92ec2e
gipsoong <ryangipsoong@gmail.com>
2024-10-02 22:52:03 +0800

    Define a function named 'getDuration'. Place prior code that calculated elapsed time of a code execution into this function (so we can call this function whenever we wanted to get the total duration).

--- main.py
+++ main.py
@@ -12,4 +12,11 @@ end = timer()
 
 # print(end-start)
 total = (timedelta(seconds=end - start)).total_seconds()
-print(f'{total}' + ' second(s)')
+print(f'#1. {total}' + ' second(s)')
+
+def getDuration():
+    duration = (timedelta(seconds=end - start)).total_seconds()
+    print(f'#2. {duration}' + ' second(s)')
+
+
+getDuration()
