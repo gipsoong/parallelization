16689463c55c71bbe506c055098372efc34c9307
gipsoong <ryangipsoong@gmail.com>
2024-10-05 23:53:04 +0800

    Write comments to demarcate original/new code. Refactor code to place original thread creation code below and out of sight. Rename 'list' to 'thread_list'. Decrease length of 'thread_list' for faster output.

--- threading_test.py
+++ threading_test.py
@@ -2,15 +2,35 @@ import logging
 import threading
 import time
 
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+thread_list = [1, 2, 3, 4, 5]
+
 
 def thread_function(name):
     logging.info("Thread %s: starting", name)
-    time.sleep(2)
+    time.sleep(1)
     logging.info("Thread %s: finishing", name)
 
 
+def loop():
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    for i in thread_list:
+        logging.info("Before creating thread")
+        x = threading.Thread(target=thread_function, args=(i,))
+        logging.info("Before running thread")
+        x.start()
+        logging.info("Wait for the thread to finish")
+        x.join()
+        logging.info("All done")
+
+
+loop()
+
+
 def main():
+    """Original code to create, start and end thread(s)."""
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
@@ -24,19 +44,3 @@ def main():
     logging.info("Main    : all done")
 
 # main()
-
-def loop():
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
-
-    for i in list:
-        logging.info("Main    : before creating thread")
-        x = threading.Thread(target=thread_function, args=(i,))
-        logging.info("Main    : before running thread")
-        x.start()
-        logging.info("Main    : wait for the thread to finish")
-        x.join()
-        logging.info("Main    : all done")
-
-loop()
