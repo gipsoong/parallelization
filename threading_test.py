d3ac59d4f5604cfb0e813bf50b22b3558cca97be
gipsoong <ryangipsoong@gmail.com>
2024-10-05 19:08:34 +0800

    **Working on single threading
    Create a list to hold an arbitrary number of values. Create a function named 'loop' to iterate through this list. Utilize and modify previous code that was creating and ending only one thread, to create x amount of threads according to the for loop. ('loop' function will be renamed to something more appropriate at a later stage.)

--- threading_test.py
+++ threading_test.py
@@ -2,6 +2,7 @@ import logging
 import threading
 import time
 
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def thread_function(name):
     logging.info("Thread %s: starting", name)
@@ -9,15 +10,33 @@ def thread_function(name):
     logging.info("Thread %s: finishing", name)
 
 
-if __name__ == "__main__":
+def main():
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
 
     logging.info("Main    : before creating thread")
-    x = threading.Thread(target=thread_function, args=(1,))
+    x = threading.Thread(target=thread_function, args=(2,))
     logging.info("Main    : before running thread")
     x.start()
     logging.info("Main    : wait for the thread to finish")
     x.join()
     logging.info("Main    : all done")
+
+# main()
+
+def loop():
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    for i in list:
+        logging.info("Main    : before creating thread")
+        x = threading.Thread(target=thread_function, args=(i,))
+        logging.info("Main    : before running thread")
+        x.start()
+        logging.info("Main    : wait for the thread to finish")
+        x.join()
+        logging.info("Main    : all done")
+
+loop()
