c627cc255389ce0a38b090f9697870eea00bbe30
gipsoong <ryangipsoong@gmail.com>
2024-10-12 22:52:28 +0800

    - Create a 'data' file to contain the items to be downloaded, instead of having them clutter up the program files.
    - Modify 'images' list to only contain strings of urls instead of dictionaries.
    
    - Create a new list named 'videos' to contain urls of videos to be downloaded.
    
    ***Working on single threaded approach
    
    - Import the lists from data.py. Create new variables with values of the aforementioned lists.
    
    - Modify main function to get the file name by splitting the url string, separating by '/' and getting the last list item [-1] (doing so because the new lists do not have a 'name' key and are just url strings).
    
    - Create new functions to mimic the code to download images. Modify these new functions to download videos instead (to test that they work with different file types).

--- before_parallelization.py
+++ before_parallelization.py
@@ -4,35 +4,10 @@ import time
 from timeit import default_timer as timer
 from datetime import timedelta
 import urllib.request
+from data import *
 
-images = [
-    {'name': 'image_1', 'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/A-Cat.jpg/2560px-A-Cat.jpg'},
-    {'name': 'image_2',
-     'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Cat_img.jpg/1280px-Cat_img.jpg'},
-    {'name': 'image_3',
-     'url': 'https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809'
-            '-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg'},
-    {'name': 'image_4',
-     'url': 'https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809'
-            '-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg'},
-    {'name': 'image_5',
-     'url': 'https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809'
-            '-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg'},
-    {'name': 'image_6',
-     'url': 'https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809'
-            '-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg'},
-    {'name': 'image_7',
-     'url': 'https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809'
-            '-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg'},
-    {'name': 'image_8',
-     'url': 'https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809'
-            '-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg'},
-    {'name': 'image_9',
-     'url': 'https://images.squarespace-cdn.com/content/v1/5452d441e4b0c188b51fef1a/1615326541809'
-            '-TW01PVTOJ4PXQUXVRLHI/male-orange-tabby-cat.jpg'},
-    {'name': 'image_10',
-     'url': 'https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg'}
-]
+list_of_images = images
+list_of_videos = videos
 
 
 def thread_function(name):
@@ -41,12 +16,17 @@ def thread_function(name):
     logging.info("Thread %s: finishing", name)
 
 
-def download_images(url, file_name, file_path):
+def download_images(image, file_name):
     full_path = 'images/' + file_name + '.jpg'
-    urllib.request.urlretrieve(url, full_path)
+    urllib.request.urlretrieve(image, full_path)
 
 
-if __name__ == '__main__':
+def download_videos(video, file_name):
+    full_path = 'videos/' + file_name + '.webm'
+    urllib.request.urlretrieve(video, full_path)
+
+
+def download_video_single_threaded():
     start = timer()
 
     format = "%(asctime)s: %(message)s"
@@ -55,13 +35,11 @@ if __name__ == '__main__':
 
     counter = 1
 
-    for i in images:
-        url = i['url']
-        file_name = i['name']
-        file_path = 'images/'
+    for i in list_of_videos:
+        file_name = i.split('/')[-1]
 
         logging.info("Before creating thread")
-        thread = threading.Thread(target=download_images, args=(url, file_name, file_path))
+        thread = threading.Thread(target=download_videos, args=(i, file_name))
         logging.info("Before running thread")
         thread.start()
         logging.info("Wait for the thread to finish")
@@ -74,3 +52,34 @@ if __name__ == '__main__':
     duration = (timedelta(seconds=end - start)).total_seconds()
 
     print(f'Entire process has finished in {duration} second(s).')
+
+
+def image():
+    start = timer()
+
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    counter = 1
+
+    for i in list_of_images:
+        file_name = i.split('/')[-1]
+
+        logging.info("Before creating thread")
+        thread = threading.Thread(target=download_images, args=(i, file_name))
+        logging.info("Before running thread")
+        thread.start()
+        logging.info("Wait for the thread to finish")
+        thread.join()
+        logging.info(f"Thread {counter} is finished.")
+
+        counter += 1
+
+    end = timer()
+    duration = (timedelta(seconds=end - start)).total_seconds()
+
+    print(f'Entire process has finished in {duration} second(s).')
+
+
+download_video_single_threaded()
---
+++ data.py
@@ -0,0 +1,21 @@
+images = [
+    'https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/640px-Cat_August_2010-4.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Domestic_cat_felis_catus.jpg/640px-Domestic_cat_felis_catus.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Domestic_cat_2019_G1.jpg/640px-Domestic_cat_2019_G1.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Curious_cat_starring_at_a_lizard.jpg/640px-Curious_cat_starring_at_a_lizard.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Domestic_cat_cropped.jpg/640px-Domestic_cat_cropped.jpg'
+
+
+
+]
+
+videos = [
+    'https://upload.wikimedia.org/wikipedia/commons/d/da/Squirrel_in_Cape_Collinson_Road_Sitting-out_Area.webm',
+    'https://upload.wikimedia.org/wikipedia/commons/9/9c/Squirrel_eating_pumpkin_seeds.webm',
+    'https://upload.wikimedia.org/wikipedia/commons/8/8e/2022-02'
+    '-11_LJUBLJANA_Krajinski_park_Tivoli_RODENTIA_Sciurus_vulgaris_NAVADNA_VEVERICA.webm',
+    'https://upload.wikimedia.org/wikipedia/commons/b/b2/Eichh%C3%B6rnchen_%28Sciurus_vulgaris_eating%29_in_Sachsen._'
+    '%D0%91%D0%B5%D0%BB%D0%BE%D1%87%D0%BA%D0%B0_%D0%B2_%D0%A1%D0%B0%D0%BA%D1%81%D0%BE%D0%BD%D0%B8%D0%B8_2H1A3145.webm',
+    'https://upload.wikimedia.org/wikipedia/commons/2/20/Squirrel_squawking_in_Central_Park_%2892123%29.webm'
+]
